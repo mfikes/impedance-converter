c055aa7b458bacd25ef4448369692cc149b4f4cd
Mike Fikes <mike@fikesfarm.com>
2023-12-23 09:35:55 -0500

    Initially generated Xcode project

---
+++ Impedance Converter/Impedance Converter/Impedance_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Impedance_ConverterApp.swift
+//  Impedance Converter
+//
+//  Created by Mike Fikes on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Impedance_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
