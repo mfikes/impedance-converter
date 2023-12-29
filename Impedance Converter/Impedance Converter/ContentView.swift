8fc809f8da0c0f09b1d276a20b1c9f5d9b6d11c2
Mike Fikes <mike@fikesfarm.com>
2023-12-29 08:11:01 -0500

    Snap to constant Smith chart lines (#17)

--- Impedance Converter/Impedance Converter/ContentView.swift
+++ Impedance Converter/Impedance Converter/ContentView.swift
@@ -823,12 +823,20 @@ struct CircuitView: View {
     }
 }
 
+enum ConstraintKind {
+    case unset, resistance, reactance, conductance, susceptance, none
+}
+
 struct SmithChartView: View {
     @ObservedObject var viewModel: ViewModel
+    
+    @State var constraintKind: ConstraintKind = .unset
+    @State var constraintValue: Double = 0
     
     var body: some View {
         GeometryReader { geometry in
             ZStack {
+                let dotRadius:CGFloat = constraintKind == .unset ? 10 : 40
                 Canvas { context, size in
                     
                     let center = CGPoint(x: size.width / 2, y: size.height / 2)
@@ -849,32 +857,21 @@ struct SmithChartView: View {
                     }
                     context.stroke(outerCircle, with: .color(.white), lineWidth: 1)
                     
+                    let gridColor: Color = constraintKind == .unset || constraintKind == .none ? .gray : Color(hex:"#FFFFFF", brightness: 0.4)
+                    
                     // Draw circles of constant resistance
                     let resistances: [Double] = [0.2, 0.5, 1, 2, 5]
                     for R in resistances {
-                        let circleRadius = radius / (R + 1)
-                        let circleCenter = CGPoint(x: center.x + radius * R / (R + 1), y: center.y)
-                        let resistanceCircle = Path { path in
-                            path.addEllipse(in: CGRect(x: circleCenter.x - circleRadius, y: circleCenter.y - circleRadius, width: 2 * circleRadius, height: 2 * circleRadius))
-                        }
-                        context.stroke(resistanceCircle, with: .color(.gray), style: dashedLineStyle)
+                        drawResistanceCircle(context: context, center: center, radius: radius, R: R, color: gridColor, style: dashedLineStyle)
                     }
+                    
+                    context.clip(to: outerCircle)
                     
                     // Draw arcs of constant reactance
                     let reactances: [Double] = [0.2, 0.5, 1, 2, 5]
                     for X in reactances {
-                        let arcRadius = radius * X
-                        let topArcCenter = CGPoint(x: center.x + radius, y: center.y - arcRadius)
-                        let bottomArcCenter = CGPoint(x: center.x + radius, y: center.y + arcRadius)
-                        let topArc = Path { path in
-                            path.addEllipse(in: CGRect(x: topArcCenter.x - arcRadius, y: topArcCenter.y - arcRadius, width: 2 * arcRadius, height: 2 * arcRadius))
-                        }
-                        let bottomArc = Path { path in
-                            path.addEllipse(in: CGRect(x: bottomArcCenter.x - arcRadius, y: bottomArcCenter.y - arcRadius, width: 2 * arcRadius, height: 2 * arcRadius))
-                        }
-                        context.clip(to: outerCircle)
-                        context.stroke(topArc, with: .color(.gray), style: dashedLineStyle)
-                        context.stroke(bottomArc, with: .color(.gray), style: dashedLineStyle)
+                        drawReactanceArc(context: context, center: center, radius: radius, X: X, color: gridColor, style: dashedLineStyle)
+                        drawReactanceArc(context: context, center: center, radius: radius, X: -X, color: gridColor, style: dashedLineStyle)
                     }
                     
                     // Draw horizontal line
@@ -882,7 +879,7 @@ struct SmithChartView: View {
                         path.move(to: CGPoint(x: center.x - radius, y: center.y))
                         path.addLine(to: CGPoint(x: center.x + radius, y: center.y))
                     }
-                    context.stroke(horizontalLine, with: .color(.gray), style: dashedLineStyle)
+                    context.stroke(horizontalLine, with: .color(gridColor), style: dashedLineStyle)
                     
                     // Plotting the center point
                     let centerPoint = CGPoint(
@@ -891,8 +888,16 @@ struct SmithChartView: View {
                     )
                     let transformedCenterPoint = transform(centerPoint)
                     let centerPointPath = Path(ellipseIn: CGRect(x: transformedCenterPoint.x - 5, y: transformedCenterPoint.y - 5, width: 10, height: 10))
-                    context.stroke(centerPointPath, with: .color(.gray))
-                    context.fill(centerPointPath, with: .color(.gray))
+                    context.stroke(centerPointPath, with: .color(gridColor))
+                    context.fill(centerPointPath, with: .color(gridColor))
+                    
+                    if (constraintKind == .resistance || constraintKind == .conductance) {
+                        drawResistanceCircle(context: context, center: center, radius: radius, R: constraintKind == .resistance ? constraintValue / viewModel.referenceImpedance.real : constraintValue * viewModel.referenceImpedance.real, color: Color(hex:"#EF8046", brightness: 1), style: StrokeStyle(lineWidth: 2, dash: [5, 5]))
+                    }
+                    
+                    if (constraintKind == .reactance || constraintKind == .susceptance) {
+                        drawReactanceArc(context: context, center: center, radius: radius, X: constraintKind == .reactance ? viewModel.referenceImpedance.real / constraintValue : -1 / (constraintValue * viewModel.referenceImpedance.real), color: Color(hex:"#EF8046", brightness: 1), style: StrokeStyle(lineWidth: 2, dash: [5, 5]))
+                    }
                 }
                 .scaleEffect(x: viewModel.smithChartDisplayMode == .admittance ? -1 : 1, y: 1, anchor: .center)
                 
@@ -915,7 +920,7 @@ struct SmithChartView: View {
                         y: viewModel.reflectionCoefficient.imaginary
                     )
                     let transformedPoint = transform(reflectionPoint)
-                    let pointPath = Path(ellipseIn: CGRect(x: transformedPoint.x - 5, y: transformedPoint.y - 5, width: 10, height: 10))
+                    let pointPath = Path(ellipseIn: CGRect(x: transformedPoint.x - dotRadius/2, y: transformedPoint.y - dotRadius/2, width: dotRadius, height: dotRadius))
                     context.stroke(pointPath, with: .color(Color(hex:"#EF8046", brightness: 1.6)))
                     context.fill(pointPath, with: .color(Color(hex:"#EF8046", brightness: 1.6)))
                 }
@@ -924,9 +929,9 @@ struct SmithChartView: View {
                     let transformedPoint = transform(reflectionCoefficient: reflectionCoefficient, size: geometry.size)
                     Circle()
                         .fill(Color(hex: "#EF8046", brightness: 1.6))
-                        .frame(width: 15, height: 15)
+                        .frame(width: 2*dotRadius, height: 2*dotRadius)
                         .position(transformedPoint)
-                        .blur(radius: 4)
+                        .blur(radius: 1.5*dotRadius)
                 }
                 
             }
@@ -935,13 +940,34 @@ struct SmithChartView: View {
                 DragGesture(minimumDistance: 0)
                     .onChanged { value in
                         let tapLocation = value.location
-                        handleTap(at: tapLocation, in: geometry.size)
+                        handleDrag(at: tapLocation, in: geometry.size)
+                    }
+                    .onEnded { _ in
+                        handleDragEnd()
                     }
             )
         }
         .aspectRatio(1, contentMode: .fit)
         .padding([.horizontal], 10)
         .padding([.bottom], 10)
+    }
+    
+    private func drawResistanceCircle(context: GraphicsContext, center: CGPoint, radius: CGFloat, R: Double, color: Color, style: StrokeStyle) {
+        let circleRadius = radius / (R + 1)
+        let circleCenter = CGPoint(x: center.x + radius * R / (R + 1), y: center.y)
+        let resistanceCircle = Path { path in
+            path.addEllipse(in: CGRect(x: circleCenter.x - circleRadius, y: circleCenter.y - circleRadius, width: 2 * circleRadius, height: 2 * circleRadius))
+        }
+        context.stroke(resistanceCircle, with: .color(color), style: style)
+    }
+    
+    private func drawReactanceArc(context: GraphicsContext, center: CGPoint, radius: CGFloat, X: Double, color: Color, style: StrokeStyle) {
+        let arcRadius = radius * X
+        let arcCenter = CGPoint(x: center.x + radius, y: center.y - arcRadius)
+        let reactanceArc = Path { path in
+            path.addEllipse(in: CGRect(x: arcCenter.x - arcRadius, y: arcCenter.y - arcRadius, width: 2 * arcRadius, height: 2 * arcRadius))
+        }
+        context.stroke(reactanceArc, with: .color(color), style: style)
     }
     
     private func transform(reflectionCoefficient: Complex, size: CGSize) -> CGPoint {
@@ -953,17 +979,22 @@ struct SmithChartView: View {
         )
     }
     
-    private func handleTap(at location: CGPoint, in size: CGSize) {
+    private func handleDrag(at location: CGPoint, in size: CGSize) {
         let center = CGPoint(x: size.width / 2, y: size.height / 2)
         let radius = min(size.width, size.height) / 2 - 20
         
-        let touchOffset = CGFloat(40) // so you can see point tapped
+        let touchOffset = CGFloat(10) // so you can see point tapped
         let tapPoint = CGPoint(
             x: (location.x - center.x) / radius,
             y: (location.y - touchOffset - center.y) / radius
         )
         
         var reflectionCoefficient = Complex(real: tapPoint.x, imaginary: -tapPoint.y)
+        
+        let resistance = viewModel.resistance
+        let reactance = viewModel.reactance
+        let conductance = viewModel.conductance
+        let susceptance = viewModel.susceptance
         
         if (reflectionCoefficient.magnitude > 1) {
             reflectionCoefficient = Complex.fromPolar(magnitude: 1, angleInRadians: reflectionCoefficient.angleInRadians)
@@ -972,6 +1003,51 @@ struct SmithChartView: View {
         } else {
             viewModel.reflectionCoefficient = reflectionCoefficient
         }
+        
+        switch constraintKind {
+        case .unset:
+            if (viewModel.smithChartDisplayMode == .admittance) {
+                if abs((viewModel.conductance - conductance)/conductance) < 0.2 {
+                    constraintKind = .conductance
+                    constraintValue = conductance
+                    viewModel.conductance = conductance
+                } else if abs((viewModel.susceptance - susceptance)/susceptance) < 0.2 ||
+                            abs(susceptance) < 0.001 && abs(viewModel.susceptance) < 0.001 {
+                    constraintKind = .susceptance
+                    constraintValue = susceptance
+                    viewModel.susceptance = susceptance
+                } else {
+                    constraintKind = .none
+                }
+            } else {
+                if abs((viewModel.resistance - resistance)/resistance) < 0.2 {
+                    constraintKind = .resistance
+                    constraintValue = resistance
+                    viewModel.resistance = resistance
+                } else if abs((viewModel.reactance - reactance)/reactance) < 0.2 ||
+                            abs(reactance) < 4 && abs(viewModel.reactance) < 4 {
+                    constraintKind = .reactance
+                    constraintValue = reactance
+                    viewModel.reactance = reactance
+                } else {
+                    constraintKind = .none
+                }
+            }
+        case .resistance:
+            viewModel.resistance = constraintValue
+        case .reactance:
+            viewModel.reactance = constraintValue
+        case .conductance:
+            viewModel.conductance = constraintValue
+        case .susceptance:
+            viewModel.susceptance = constraintValue
+        case .none:
+            break
+        }
+    }
+    
+    private func handleDragEnd() {
+        constraintKind = .unset
     }
 }
 
