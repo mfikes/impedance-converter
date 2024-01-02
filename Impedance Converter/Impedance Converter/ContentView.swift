bc979bb1575c064f7e9072e117241bee6524f1bb
Mike Fikes <mike@fikesfarm.com>
2024-01-01 19:17:43 -0500

    Add a characterization view below Smith chart (#66)

--- Impedance Converter/Impedance Converter/ContentView.swift
+++ Impedance Converter/Impedance Converter/ContentView.swift
@@ -22,27 +22,52 @@ struct ContentView: View {
     
     @StateObject var viewModel = ViewModel()
     
-    init() {
-        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(Color.baseSegmentControlTintColor.adjusted(brightness: 1.2)
-        )
-    }
-    
     var body: some View {
-        ZStack {
-            Color.baseAppBackgroundColor.edgesIgnoringSafeArea(.all)
-            ScrollView {
-                VStack {
-                    VStack {
-                        ComplexImpedanceView(viewModel: viewModel)
-                        ParametersView(viewModel: viewModel)
-                        CircuitView(viewModel: viewModel)
+        GeometryReader { geometry in
+            ZStack {
+                Color.baseAppBackgroundColor.edgesIgnoringSafeArea(.all)
+                if geometry.size.width > geometry.size.height {
+                    HStack(alignment: .top, spacing: 10) {
+                        ImpedanceColumnView(viewModel: viewModel)
+                        SmithChartColumnView(viewModel: viewModel)
                     }
-                    SmithChartView(viewModel: viewModel)
+                    .padding(10)
+                } else {
+                    ScrollView {
+                        VStack {
+                            Spacer(minLength: 0)
+                            ImpedanceColumnView(viewModel: viewModel)
+                            SmithChartColumnView(viewModel: viewModel)
+                            Spacer(minLength: 0)
+                        }
+                        .frame(minHeight: geometry.size.height)
+                    }
+                    .frame(maxWidth: 500)
                 }
             }
-            .frame(maxWidth: 500)
-            .padding(.top, 1)
             .dynamicTypeSize(.medium)
+        }
+    }
+}
+
+
+struct ImpedanceColumnView: View {
+    var viewModel: ViewModel
+    var body: some View {
+        VStack {
+            ComplexImpedanceView(viewModel: viewModel)
+            ParametersView(viewModel: viewModel)
+            CircuitView(viewModel: viewModel)
+        }
+    }
+}
+
+struct SmithChartColumnView: View {
+    var viewModel: ViewModel
+    var body: some View {
+        VStack {
+            SmithChartView(viewModel: viewModel)
+            CharacterizationView(viewModel: viewModel)
         }
     }
 }
