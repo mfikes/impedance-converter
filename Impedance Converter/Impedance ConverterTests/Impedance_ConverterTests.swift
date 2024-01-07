0b8670a599817b43b7145e2eae1c94e4e2d060c8
Mike Fikes <mike@fikesfarm.com>
2024-01-07 13:40:45 -0500

    Squashed commit of the following:
    commit b87f31e876d7ea5ec2b2b48dd7085566d07b3f02
    Author: Mike Fikes <mike@fikesfarm.com>
    Date:   Sun Jan 7 13:40:12 2024 -0500
    
        Add tests (initially failing)

---
+++ Impedance Converter/Impedance ConverterTests/Impedance_ConverterTests.swift
@@ -0,0 +1,31 @@
+import XCTest
+
+final class Impedance_ConverterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case that is intentionally designed to fail.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        // This assertion will always fail because the condition is always true.
+        XCTAssertFalse(true, "This assertion is intended to fail")
+
+        // Alternatively, you can use XCTAssertTrue with a condition that's always false.
+        // XCTAssertTrue(false, "This assertion is also intended to fail")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
