8fab65dab2393c983d00ccccb98a77001b9df058
Mike Fikes <mike@fikesfarm.com>
2024-01-11 22:48:28 -0500

    Remove desd sin / cos code (#127)

--- Impedance Converter/Impedance Converter/Math.swift
+++ Impedance Converter/Impedance Converter/Math.swift
@@ -19,36 +19,6 @@ func symmetricRemainder(dividend: Double, divisor: Double) -> Double {
     return result == 0 ? 0 * dividend : result
 }
 
-func cos(angle: Angle) -> Double {
-    let normalizedAngle = angle.degrees.truncatingRemainder(dividingBy: 360)
-    
-    switch abs(normalizedAngle) {
-    case 90, 270:
-        return 0
-    case 180:
-        return -1
-    case 0, 360:
-        return 1
-    default:
-        return Darwin.cos(angle.radians)
-    }
-}
-
-func sin(angle: Angle) -> Double {
-    let normalizedAngle = angle.degrees.truncatingRemainder(dividingBy: 360)
-    
-    switch normalizedAngle {
-    case -270, 90:
-        return 1
-    case 270, -90:
-        return -1
-    case 0, 180, -180, 360:
-        return 0
-    default:
-        return Darwin.sin(angle.radians)
-    }
-}
-
 extension Complex where RealType: FloatingPoint {
     var canonicalizedReal: RealType {
         return isFinite ? real : RealType.infinity
